e614aaa415428e4a8bf0a8641cba7896cc6c81c0
Vikrant7981 <70353912+Vikrant7981@users.noreply.github.com>
2020-09-17 13:35:11 +0530

    Update q1.py

--- q1/q1.py
+++ q1/q1.py
@@ -1,4 +1,10 @@
 import functools as ft
+from itertools import chain
 
 def Fib(n):
-    ## write your code here
+  result = list(range(2, n+1))
+  temp = result
+  temp = list( map(lambda v : list( filter(lambda w : w !=v and w % v == 0 , temp) ), temp))
+  temp = list(set(chain(*temp)))
+  result = list(filter(lambda x: x not in temp, result))
+  return result
