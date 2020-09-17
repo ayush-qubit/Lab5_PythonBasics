a0f891647776a33bb05fbaa452c7c255c1cc4e21
Vikrant7981 <70353912+Vikrant7981@users.noreply.github.com>
2020-09-17 18:30:03 +0530

    Update q1.py

--- q1/q1.py
+++ q1/q1.py
@@ -3,7 +3,7 @@ from itertools import chain
 
 def Fib(n):
   result = list(range(2, n+1))
-  temp = result
+  temp = result.copy()
   temp = list( map(lambda v : list( filter(lambda w : w !=v and w % v == 0 , temp) ), temp))
   temp = list(set(chain(*temp)))
   result = list(filter(lambda x: x not in temp, result))
