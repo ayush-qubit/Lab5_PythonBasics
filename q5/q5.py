085f2ad84d593f153b9e4d64105169683609e946
ayush-qubit <37446683+ayush-qubit@users.noreply.github.com>
2020-09-17 16:55:49 +0530

    Update q5.py

--- q5/q5.py
+++ q5/q5.py
@@ -1,76 +1,24 @@
-from functools import reduce 
-class BlackMoneyHolder:
-    def __init__(self,name,account_info):
-        self.name=name
-        self.account_info=account_info
-        self.index=0
+if __name__ == "__main__":
+    d1={}
+    d2={}
+    n=int(input())
+    for _ in range(n):
+        match,players=input().split(sep=':')
+        players=list(players.split(sep=','))
+        for player in players:
+            name,score=player.split(sep='-')
+            if match in d1:
+                d1[match][name]=score
+            else:
+                d1[match]={}
+                d1[match][name]=score
+            if name in d2:
+                t=int(d2[name])
+                t+=int(score)
+                d2[name]=t
+            else:
+                d2[name]=score
     
-    def update_amount(self,bank_name,amount):
-        if bank_name in self.account_info:
-            self.account_info[bank_name]=self.account_info[bank_name]+amount
-        else:
-            self.account_info[bank_name]=amount
-    
-    def total_black_money(self):
-        return reduce(lambda x,y: x+y,self.account_info.values())
-    
-    def __iter__(self):
-        return self
-    
-    def __next__(self):
-        l=len(self.account_info)
-        if self.index<l:
-            key=list(self.account_info)[self.index]
-            value=list(self.account_info.values())[self.index]
-            self.index+=1
-            return (key,value)
-        raise StopIteration
-
-    def __str__(self):
-        string=''
-        for ele in self.account_info:
-            string+=str(ele)+':'+str(self.account_info[ele])+'\n'
-        return string
-    
-    
-    def __eq__(self, other):
-        a=reduce(lambda x,y: x+y,self.account_info.values())
-        b=other.total_black_money()
-        return a==b
-    
-    def __lt__(self, other):
-        a=reduce(lambda x,y: x+y,self.account_info.values())
-        b=other.total_black_money()
-        return a<b
-    
-    def __gt__(self, other):
-        a=reduce(lambda x,y: x+y,self.account_info.values())
-        b=other.total_black_money()
-        return a>b
-    
-    def __le__(self, other):
-        a=reduce(lambda x,y: x+y,self.account_info.values())
-        b=other.total_black_money()
-        return a<=b
-    
-    def __ge__(self, other):
-        a=reduce(lambda x,y: x+y,self.account_info.values())
-        b=other.total_black_money()
-        return a>=b
-
-
-name='Sonia Gandhi'
-account_info={
-    'A':100,
-    'B':200,
-    'C':300
-}
-B1=BlackMoneyHolder(name,account_info)
-print(B1)
-account_info1={
-    'A':100,
-    'B':200,
-    'C':300
-}
-B2=BlackMoneyHolder('Rahul',account_info1)
-print(B1==B2,B1<B2,B1>B2,B1>=B2,B1<=B2)
+    print(d1)
+    l=list(d2.items())
+    print(l)
