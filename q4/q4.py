b1cb444a61e35f643997e2456b0aadb281d0c279
Vikrant7981 <70353912+Vikrant7981@users.noreply.github.com>
2020-09-17 13:35:51 +0530

    Update q4.py

--- q4/q4.py
+++ q4/q4.py
@@ -1,5 +1,28 @@
 import itertools as it
-# import operator as op
+#import operator as op
 
 def myFunc(deg, vals, coeffs):
-    ## write your code here
+  temp = coeffs
+  val1= vals[0]
+  val2= vals[1]
+  iter = 0
+
+  while(iter<=deg):
+    holder = coeffs[iter] * (val2**iter)*(val1**(deg-iter))
+    listxy = ""
+    listx = 'x*' * (deg-iter)
+    listx = listx[0:-1]
+    listy = 'y*' * (iter)
+    listy = listy[0:-1]
+    if listx and listy:
+      listxy = listx+'*'+listy
+    elif listx:
+      listxy = listx
+    else:
+      listxy = listy
+    temp[iter] = listxy + ' -> ' + str(holder)
+    iter = iter +1
+
+  
+  return temp
+
