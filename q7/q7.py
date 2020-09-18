c3a863ba97cb721a7474273d1d3ce2357eeed77b
Ayush <ayushsharma1198@gmail.com>
2020-09-18 18:52:51 +0530

    q7 done

--- q7/q7.py
+++ q7/q7.py
@@ -3,15 +3,16 @@ import sys
 def get_inputs(path):
     result=[]
     with open(path,'r') as file:
-        result.append(file.readline())
+        result=file.readlines()
     return result
 
 if __name__ == "__main__":
-    file_name=sys.argv[0]
+    file_name=sys.argv[1]
     result=get_inputs(file_name)
+    #print(result)
     T=int(result[0])
-    for i in T:
-        H,M,S,a,b,c=result[i+1].split(sep=' ')
+    for i in range(T):
+        H,M,S,a,b,c=map(int,result[i+1].split(sep=' '))
         hour_angle=(360/H)*a+(360/(H*M))*b+(360/(H*M*S))*c
         minute_angle=(360/M)*b+(360/(M*S))*c
         diff_angle=abs(float(hour_angle)-float(minute_angle))
