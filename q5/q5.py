77399d5500e11877a752a096f3fdaeb9f580d934
Ayush <ayushsharma1198@gmail.com>
2020-09-19 15:22:16 +0530

    q5 updated

--- q5/q5.py
+++ q5/q5.py
@@ -8,17 +8,17 @@ if __name__ == "__main__":
         for player in players:
             name,score=player.split(sep='-')
             if match in d1:
-                d1[match][name]=score
+                d1[match][name]=int(score)
             else:
                 d1[match]={}
-                d1[match][name]=score
+                d1[match][name]=int(score)
             if name in d2:
                 t=int(d2[name])
                 t+=int(score)
                 d2[name]=t
             else:
-                d2[name]=score
+                d2[name]=int(score)
     
     print(d1)
     l=list(d2.items())
-    print(l)
+    print(sorted(l,key=lambda x: x[1],reverse=True))
